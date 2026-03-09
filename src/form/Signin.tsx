a396894ec99204ccdecb10dc53c1897253573085
Tatiana <kartasheva.tatiana@gmail.com>
2026-03-09 19:47:34 +0300

    feat: add sing in logic

--- src/form/Signin.tsx
+++ src/form/Signin.tsx
@@ -1,18 +1,46 @@
-export function Signin() {
+import { useRef, type ChangeEvent, type ChangeEventHandler, type SubmitEventHandler } from "react";
+
+interface SigninProps {
+    onSubmit: (arg: {}) => void
+};
+
+export function Signin({onSubmit}: SigninProps) {
+    const inputs = useRef({
+        email: '',
+        password: ''
+    });
+
+    const handleChange: ChangeEventHandler = (event: ChangeEvent) => {
+        const target = event.target as HTMLInputElement;
+
+        inputs.current = {
+            ...inputs.current,
+            [target.name]: target.value
+        };
+    };
+
+    const handleSubmit: SubmitEventHandler = (event) => {
+        event.preventDefault();
+        onSubmit(inputs.current);
+    };
+
     return (
         <>
             <h2>Авторизация</h2>
-            <form>
+            <form
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            >
                 <div>
                     <input
                         type="email"
-                        name="EMAIL"
+                        name="email"
                     />
                 </div>
                 <div>
                     <input
                         type="password"
-                        name="PASSWORD"
+                        name="password"
                     />
                 </div>
                 <button type="submit">Войти</button>
