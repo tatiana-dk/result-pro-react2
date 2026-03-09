e9db68d23f7e2fc576a2cb817284460a301e05df
Tatiana <kartasheva.tatiana@gmail.com>
2026-03-09 19:06:51 +0300

    feat: add signin markup

--- src/form/Signin.tsx
+++ src/form/Signin.tsx
@@ -1,5 +1,19 @@
 export function Signin() {
     return (
-        <></>
+        <form>
+            <div>
+                <input
+                    type="email"
+                    name="EMAIL"
+                />
+            </div>
+            <div>
+                <input
+                    type="password"
+                    name="PASSWORD"
+                />
+            </div>
+            <button type="submit">Войти</button>
+        </form>
     );
 }
