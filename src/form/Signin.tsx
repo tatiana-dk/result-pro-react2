8870b2cf2952b35911105aeca028260783903a3c
Tatiana <kartasheva.tatiana@gmail.com>
2026-03-09 19:15:22 +0300

    feat: add h2

--- src/form/Signin.tsx
+++ src/form/Signin.tsx
@@ -1,19 +1,22 @@
 export function Signin() {
     return (
-        <form>
-            <div>
-                <input
-                    type="email"
-                    name="EMAIL"
-                />
-            </div>
-            <div>
-                <input
-                    type="password"
-                    name="PASSWORD"
-                />
-            </div>
-            <button type="submit">Войти</button>
-        </form>
+        <>
+            <h2>Авторизация</h2>
+            <form>
+                <div>
+                    <input
+                        type="email"
+                        name="EMAIL"
+                    />
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        name="PASSWORD"
+                    />
+                </div>
+                <button type="submit">Войти</button>
+            </form>
+        </>
     );
 }
